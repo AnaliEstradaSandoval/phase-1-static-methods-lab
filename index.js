a86e4a0515682ca56ce84f36b39892f8bab7b23e
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-06-05 18:03:50 -0700

    static method lab complete

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+
+  static titleize(string){
+    const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const words = string.split(' ');
+    for(let i = 0; i < words.length; i++){
+     const word = words[i];
+     if(i === 0 || !exceptions.includes(word)){
+      words[i] = this.capitalize(word);
+     }
+  }
+  return words.join(' ')
 }
+}
+
+
+
+
+// static titleize(string){
+//   const exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+//   string = string.toLowerCase().split(' ');
+//   for(let i = 0; i < string.length; i++){
+//     string[i] = string[i].charAt(0).toUpperCase() + string[i].slice(1);
+//     if(string.includes(exceptions)){
+//       return string.toLowercase();
+//     }
+//   }
+//   return string.join(' ')
+// }
+// }
